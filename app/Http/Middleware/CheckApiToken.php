f2fc7567b5969830fe497aaddbc6e166a5c0340c
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-02-01 15:34:02 +0100

    Creazione middleware + controllo sul token api con il middelware

---
+++ app/Http/Middleware/CheckApiToken.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\User;
+
+class CheckApiToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $auth_token = $request->header('authorization');
+        if(empty($auth_token)) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api token mancante'
+            ]);
+        }
+        $api_token = substr($auth_token, 7);
+        $user = User::where('api_token', $api_token)->first();
+        if(!$user) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api token errato'
+            ]);
+        }
+        return $next($request);
+    }
+}
